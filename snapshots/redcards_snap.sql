330d1be7595bb81b2734d1359110649bb3f06ef0
karey.graham@matillion.com <karey.graham@matillion.com>
2023-03-27 16:27:35 +0000

    bare check

--- snapshots/redcards_snap.sql
+++ snapshots/redcards_snap.sql
@@ -1,16 +1,4 @@
 {% snapshot redcards_snapshot_check %}
 
---   {{
---       config(
---          target_schema= "PUBLIC",
---          strategy='check',
---          unique_key='FIRSTNAME',
---          check_cols=['FIRSTNAME', 'LASTNAME','YELLOW', 'RED']
---        )
---    }}
-
---SELECT
---  *
---FROM {{source('redcards_structured','redcards_structured')}}
 
 {% endsnapshot %}
