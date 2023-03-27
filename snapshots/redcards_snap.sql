72404ef79a0ee30e80d457e4dfc89d13ce63d5c6
karey.graham@matillion.com <karey.graham@matillion.com>
2023-03-27 16:26:20 +0000

    bare check

--- snapshots/redcards_snap.sql
+++ snapshots/redcards_snap.sql
@@ -1,16 +1,16 @@
-{% snapshot redcards_snapshot %}
+{% snapshot redcards_snapshot_check %}
 
-    {{
-        config(
-          target_schema= "PUBLIC",
-          strategy='check',
-          unique_key='FIRSTNAME',
-          check_cols=['FIRSTNAME', 'LASTNAME','YELLOW', 'RED'],
-        )
-    }}
+--   {{
+--       config(
+--          target_schema= "PUBLIC",
+--          strategy='check',
+--          unique_key='FIRSTNAME',
+--          check_cols=['FIRSTNAME', 'LASTNAME','YELLOW', 'RED']
+--        )
+--    }}
 
-SELECT
-  *
-FROM {{source('redcards_structured','redcards_structured')}}
+--SELECT
+--  *
+--FROM {{source('redcards_structured','redcards_structured')}}
 
 {% endsnapshot %}
