14cf72cfce50ff330ad375f27738c47f8fe87a8f
karey.graham@matillion.com <karey.graham@matillion.com>
2023-04-12 18:36:18 +0000

    update model

--- models/redcards/DBTMODEL_PREMIERCARDS.sql
+++ models/redcards/DBTMODEL_PREMIERCARDS.sql
@@ -1,5 +1,6 @@
 SELECT
   *,
+  firstname || ' ' || lastname fullname,
   (yellow + red) sumcards,
   ((yellow + red)/minutes) cards_per_minute
 FROM {{source('redcards_source','STG_SPRING_PREMIERCARDS')}}
