06fe320ed9c463368b2c44c6d8bf581db15025df
karey.graham@matillion.com <karey.graham@matillion.com>
2023-04-12 19:45:30 +0000

    snapshot

---
+++ snapshots/premiercardssnapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot redcards_snapshot_check %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='firstname',
+        strategy='timestamp',
+        updated_at='lastupdated',
+    )
+}}
+
+SELECT
+  *
+FROM {{source('redcards_source','STG_SPRING_PREMIERCARDS')}}
+
+{% endsnapshot %}
